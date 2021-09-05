5aeee72623ca3f995f13f4ea2abd968f69d59bac
Hevesh Lakhwani <hevesh000@gmail.com>
2021-09-05 20:52:23 +0530

    Cleaned Web and Windows files

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
 class AuthScreen extends StatelessWidget {
-  const AuthScreen({ Key? key }) : super(key: key);
+  const AuthScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(child: Text("AuthScreen!"),),
+      body: Center(
+        child: Text("AuthScreen!"),
+      ),
     );
   }
 }
