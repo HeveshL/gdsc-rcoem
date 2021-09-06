79d8af2163f310b1f8b653b22e1262030ca7efd5
Shubham Joshi <joshishubham0301@gmail.com>
2021-09-06 10:11:51 +0530

    hello auth

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,13 +1,148 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class AuthScreen extends StatelessWidget {
+class AuthScreen extends StatefulWidget {
   const AuthScreen({Key? key}) : super(key: key);
 
   @override
+  _AuthScreenState createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  final TextEditingController _phoneNoController = TextEditingController();
+  bool _loading = false;
+
+  Future signIn() async {
+    FirebaseAuth.instance.verifyPhoneNumber(
+        phoneNumber: "+91${_phoneNoController.text}",
+        verificationCompleted: (PhoneAuthCredential credential) async {
+          // ANDROID ONLY!
+
+          // Sign the user in (or link) with the auto-generated credential
+          await FirebaseAuth.instance.signInWithCredential(credential);
+        },
+        verificationFailed: (FirebaseAuthException e) {
+          if (e.code == 'invalid-phone-number') {
+            //TODO: Add toast
+            print('The provided phone number is not valid.');
+          }
+
+          // Handle other errors
+        },
+        codeSent: (String verificationId, int? resendToken) async {
+          // Update the UI - wait for the user to enter the SMS code
+          String smsCode = 'xxxx';
+
+          // Create a PhoneAuthCredential with the code
+          PhoneAuthCredential credential = PhoneAuthProvider.credential(
+              verificationId: verificationId, smsCode: smsCode);
+
+          // Sign the user in (or link) with the credential
+          await FirebaseAuth.instance.signInWithCredential(credential);
+        },
+        codeAutoRetrievalTimeout: (String verificationId) {
+          // Auto-resolution timed out...
+        });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("AuthScreen!"),
+    return Scaffold(
+      body: SizedBox.expand(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: [
+                    BoxShadow(
+                      offset: const Offset(0, 3),
+                      blurRadius: 6,
+                      color: const Color(0xff000000).withOpacity(0.16),
+                    ),
+                  ],
+                ),
+                height: 60,
+                child: TextField(
+                  textInputAction: TextInputAction.done,
+                  key: UniqueKey(),
+                  controller: _phoneNoController,
+                  keyboardType: TextInputType.phone,
+                  style: const TextStyle(
+                    // fontFamily: "Nunito",
+                    fontSize: 16,
+                    color: Colors.black,
+                  ),
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    // hintText: "Eter your phone number",
+                    labelText: "Phone number",
+                    prefixText: "+91",
+                    filled: true,
+                    fillColor: Colors.transparent,
+                    labelStyle: TextStyle(
+                      // fontFamily: "Nunito",
+                      fontSize: 16,
+                      color: Colors.black.withOpacity(0.58),
+                    ),
+                  ),
+                  cursorColor: Colors.black,
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                    onTap: () async {
+                      await signIn();
+                    },
+                    child: Container(
+                      height: 50,
+                      width: 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.blue,
+                        boxShadow: [
+                          BoxShadow(
+                            offset: const Offset(0, 3),
+                            blurRadius: 6,
+                            color: const Color(0xff000000).withOpacity(0.16),
+                          ),
+                        ],
+                      ),
+                      child: Center(
+                        child: _loading
+                            ? const SizedBox(
+                                height: 15,
+                                width: 15,
+                                child: CircularProgressIndicator(
+                                  strokeWidth: 2,
+                                  color: Colors.cyan,
+                                ),
+                              )
+                            : const Text(
+                                "Login",
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  // fontFamily: "Nunito",
+                                  color: Colors.white,
+                                ),
+                              ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
